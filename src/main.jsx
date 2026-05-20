a2242219f124c618b5498e2cee78b43e4897652b
davidbedoya <davidbedoya1000@gmail.com>
2026-05-20 13:04:08 -0400

    Revert router changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // import Main from "./components/Main/Main";
 import "./index.css";
 import App from "./App.jsx";
-// import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <BrowserRouter>
+    <BrowserRouter>
       <App />
-    </BrowserRouter> */}
-    <HashRouter>
-      <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
