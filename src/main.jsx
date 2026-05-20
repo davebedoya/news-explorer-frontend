1dbd6fed31324e8331c1401d28d5e036665f336a
davidbedoya <davidbedoya1000@gmail.com>
2026-05-20 12:56:55 -0400

    feat: utilizing hashrouter instead of browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // import Main from "./components/Main/Main";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 );
